13dee02ece459c916582e5779a6cf5b18ea72894
SidenkoStas <volondss@gmail.com>
2025-04-15 15:56:34 +0300

    Создан проект социальной сети. Добавлена возможность входить и выходить в свой профиль. Добавлены шаблоны страниц для входа/выхода из личного каьинета сайта.

---
+++ account/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+class LoginForm(forms.Form):
+    """
+    Форма для авторизации пользователей.
+    """
+    username = forms.CharField(label="Имя пользователя")
+    password = forms.CharField(widget=forms.PasswordInput, label="Пароль")
---
+++ account/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from .forms import LoginForm
+
+def user_login(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(
+                request, username=cd["username"], password=cd["password"]
+            )
+            if user is not None:
+                login(request, user)
+                return HttpResponse("Успешная авторизация.")
+            else:
+                return HttpResponse("Такого пользователя не существует!")
+        else:
+            return HttpResponse("Не правильный логин или пароль!")
+    else:
+        form = LoginForm()
+    return render(
+        request, "account/login.html", {"form": form}
+    )
+
+@login_required
+def dashboard(request):
+    return render(
+        request, "account/dashboard.html",
+        {"section": "dashboard"}
+    )
